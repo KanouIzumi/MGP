a9516274632344d291bff7946980832252637092
KanouIzumi <72852474+KanouIzumi@users.noreply.github.com>
2021-04-21 11:47:00 +0800

    Lab 5

---
+++ 28_Lab5_ShanQing/Assets/Script/UIScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour
+{
+    public Text UIScoreText;
+    int score = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UIScoreText.text = "Score: " + score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ScoreIncrement()
+    {
+        score += 1;
+        UIScoreText.text = "Score: " + score.ToString();
+    }
+}
