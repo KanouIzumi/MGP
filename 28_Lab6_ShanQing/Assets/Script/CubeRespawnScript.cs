19ff9f622bc02777db525eee8553c05da96d9646
KanouIzumi <72852474+KanouIzumi@users.noreply.github.com>
2021-04-21 14:22:30 +0800

    LS6

---
+++ 28_Lab6_ShanQing/Assets/Script/CubeRespawnScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRespawnScript : MonoBehaviour
+{
+
+    public UIScript UIScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // for mobile devices
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                SpawnCubes();
+                UIScore.ScoreIncrement();
+                Destroy(hit.collider.gameObject);
+            }
+        }
+
+        //// for PC 
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //    RaycastHit hit;
+
+        //    if (Physics.Raycast(ray, out hit))
+        //    {
+        //        SpawnCubes();
+        //        UIScore.ScoreIncrement();
+        //        Destroy(hit.collider.gameObject);
+        //    }
+        //}
+    }
+
+    void SpawnCubes()
+    {
+        Vector3 position = new Vector3(Random.Range(-4f, 4f), Random.Range(-5f, 5f));
+        Instantiate(gameObject, position, Quaternion.identity);
+    }
+}
---
+++ 28_Lab6_ShanQing/Assets/Script/UIScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIScript : MonoBehaviour
+{
+    public Text UIScoreText;
+    int score = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UIScoreText.text = "Score: " + score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(score >=5)
+        {
+            SceneManager.LoadScene("WinScene");
+        }
+    }
+
+    public void ScoreIncrement()
+    {
+        score += 1;
+        UIScoreText.text = "Score: " + score.ToString();
+    }
+}
